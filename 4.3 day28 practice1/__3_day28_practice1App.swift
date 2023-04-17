1c3f6785a2783165a331c444dc95de4a7e482341
lee <lee>
2023-04-17 16:15:59 +0900

    Add Recipe TabView.swift as the starting point for the app

--- 4.3 day28 practice1/__3_day28_practice1App.swift
+++ 4.3 day28 practice1/__3_day28_practice1App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct __3_day28_practice1App: App {
     var body: some Scene {
         WindowGroup {
-            RecipeListView()
+            RecipeTabView()
         }
     }
 }
